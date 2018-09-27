ca34fa8327e4b8dbaeebe565ad1321bc2e8cdf86
zhouwei <342374831@qq.com>
2018-09-27 21:12:42 +0800

    公共头部完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -4,3 +4,37 @@ $(document).ajaxStart(function () {
 $(document).ajaxStart(function () {
   NProgress.done();
 });
+
+// 公共的效果
+$(function(){
+  // 二级菜单切换效果
+  $(".lt_aside .category").click(function(){
+    $(".lt_aside .child").stop().slideToggle();
+  })
+
+  // 左侧菜单栏切换
+  $(".icon_menu").click(function(){
+    $(".lt_aside").toggleClass("hidemenu");
+    $(".lt_topbal").toggleClass("hidemenu");
+    $(".lt_main").toggleClass("hidemenu");
+  })
+})
+
+
+$(function(){
+  $(".icon_logout").click(function(){
+    $("#logoutModal").modal("show");
+  })
+   $("#logoutBtn").click(function(){
+    $.ajax({
+      type:"get",
+      url:"/employee/employeeLogout",
+      dataType:"json",
+      success:function(info){
+        if(info.success){
+          location.href = "login.html"
+        }
+      }
+    })
+   })
+  })
